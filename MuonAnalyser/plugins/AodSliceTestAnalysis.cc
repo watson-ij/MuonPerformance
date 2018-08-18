07e585df407d2dd584ae3c4549d91c289c6c90e0
Ian J. Watson <ian.james.watson@cern.ch>
2018-08-18 16:45:07 +0900

    Add muon information to AodSliceTestAnalysis

--- MuonAnalyser/plugins/AodSliceTestAnalysis.cc
+++ MuonAnalyser/plugins/AodSliceTestAnalysis.cc
@@ -76,16 +76,6 @@ private:
   edm::ESHandle<TransientTrackBuilder> ttrackBuilder_;
   edm::ESHandle<MagneticField> bField_; 
   
-  TH2D* h_firstStrip[36][3];
-  TH2D* h_allStrips[36][3];
-  TH2D* h_globalPosOnGem;
-  TH1D* h_clusterSize, *h_totalStrips, *h_bxtotal;
-  TH1D* h_inEta[36][3];
-  TH1D* h_hitEta[36][3];
-  TH1D* h_trkEta[36][3];
-
-  TH1D* h_res_x, *h_res_y, *h_pull_x, *h_pull_y;
-
   int b_run, b_lumi, b_event;
   int b_firstStrip, b_nStrips, b_chamber, b_layer, b_etaPartition, b_muonQuality, b_bx;
   vector<int> b_strips;
@@ -111,7 +101,8 @@ private:
   vector<int> m_in_roll, m_in_chamber, m_in_layer; // propagation bound info
   vector<float> m_in_globalPhi, m_in_globalEta, m_in_nearGemPhi, m_in_nearGemEta; // global info
   vector<float> m_in_x, m_in_y, m_in_gemx, m_in_gemy, m_in_pullx, m_in_pully, m_in_resx, m_in_resy;
-  vector<int> m_in_nearGemNStrips, m_in_nearGemFirstStrip;
+  vector<int> m_in_gemNStrips, m_in_gemFirstStrip;
+  vector<bool> m_in_matchingGem;
   
   TTree *t_hit;
   TTree *t_run;
@@ -128,6 +119,8 @@ AodSliceTestAnalysis::AodSliceTestAnalysis(const edm::ParameterSet& iConfig) :
 { 
   gemRecHits_ = consumes<GEMRecHitCollection>(iConfig.getParameter<edm::InputTag>("gemRecHits"));
   muons_ = consumes<View<reco::Muon> >(iConfig.getParameter<InputTag>("muons"));
+  edm::ParameterSet serviceParameters = iConfig.getParameter<edm::ParameterSet>("ServiceParameters");
+  theService_ = new MuonServiceProxy(serviceParameters);
   
   t_event = fs->make<TTree>("Event", "Event");
   t_event->Branch("nMuons", &b_nMuons, "nMuons/I");
@@ -135,6 +128,36 @@ AodSliceTestAnalysis::AodSliceTestAnalysis(const edm::ParameterSet& iConfig) :
   t_event->Branch("nGEMHits", &b_nGEMHits, "nGEMHits/I");
   t_event->Branch("run", &b_run, "run/I");
   t_event->Branch("lumi", &b_lumi, "lumi/I");
+
+  t_muon = fs->make<TTree>("Muon", "Muon");
+  t_muon->Branch("nhits", &m_nhits, "nhits/I")->SetTitle("n GEM hits associated to muon");
+  t_muon->Branch("nvalidhits", &m_nvalidhits, "nvalidhits/I")->SetTitle("n GEM hits associated to muon, and muon can propagate to eta partition of hit");
+  t_muon->Branch("nbounds", &m_nbounds, "nbounds/I")->SetTitle("times muon is in GEM eta partition bounds");
+  t_muon->Branch("pt", &m_pt, "pt/F");
+  t_muon->Branch("eta", &m_eta, "eta/F");
+  t_muon->Branch("phi", &m_phi, "phi/F");
+  t_muon->Branch("quality", &m_quality, "quality/I")->SetTitle("muon quality :: 0:noid 1:looseID 2:tightID");
+  t_muon->Branch("in_roll", &m_in_roll);
+  t_muon->Branch("in_chamber", &m_in_chamber);
+  t_muon->Branch("in_layer", &m_in_layer);
+  t_muon->Branch("in_resx", &m_in_resx);
+  t_muon->Branch("in_resy", &m_in_resy);
+  t_muon->Branch("in_pullx", &m_in_pullx);
+  t_muon->Branch("in_pully", &m_in_pully);
+  t_muon->Branch("in_roll", &m_in_roll);
+  t_muon->Branch("in_chamber", &m_in_chamber);
+  t_muon->Branch("in_layer", &m_in_layer);
+  t_muon->Branch("in_phi", &m_in_globalPhi);
+  t_muon->Branch("in_eta", &m_in_globalEta);
+  t_muon->Branch("in_nearGemPhi", &m_in_nearGemPhi);
+  t_muon->Branch("in_nearGemEta", &m_in_nearGemEta);
+  t_muon->Branch("in_x", &m_in_x);
+  t_muon->Branch("in_y", &m_in_y);
+  t_muon->Branch("in_gemx", &m_in_gemx);
+  t_muon->Branch("in_gemy", &m_in_gemy);
+  t_muon->Branch("in_gemFirstStrip", &m_in_gemFirstStrip);
+  t_muon->Branch("in_gemNStrips", &m_in_gemNStrips);
+  t_muon->Branch("in_matchingGem", &m_in_matchingGem);
 
   t_hit = fs->make<TTree>("Hit", "Hit");
   t_hit->Branch("run", &b_run, "run/I");
@@ -167,6 +190,9 @@ AodSliceTestAnalysis::~AodSliceTestAnalysis()
 void
 AodSliceTestAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+  edm::ESHandle<GEMGeometry> GEMGeometry_;
+  iSetup.get<MuonGeometryRecord>().get(GEMGeometry_);
+
   edm::Handle<GEMRecHitCollection> gemRecHits;  
   iEvent.getByToken(gemRecHits_, gemRecHits);
 
@@ -174,6 +200,8 @@ AodSliceTestAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& i
   iEvent.getByToken(muons_, muons);
 
   iSetup.get<TransientTrackRecord>().get("TransientTrackBuilder", ttrackBuilder_);
+  theService_->update(iSetup);
+  auto propagator = theService_->propagator("SteppingHelixPropagatorAny");
 
   nEvents++;
 
@@ -186,22 +214,88 @@ AodSliceTestAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& i
 
   for (auto & mu : *muons) {
     b_nMuons++;
+    m_in_roll.clear();
+    m_in_chamber.clear();
+    m_in_layer.clear();
+    m_nhits = 0;
+    m_nbounds = 0;
+    m_in_x.clear(); m_in_y.clear(); m_in_gemx.clear(); m_in_gemy.clear();
+    m_in_resx.clear(); m_in_resy.clear(); m_in_pullx.clear(); m_in_pully.clear();
+    m_in_matchingGem.clear();
+    m_in_gemFirstStrip.clear(); m_in_gemNStrips.clear();
     
     if (mu.passed(reco::Muon::Selector::CutBasedIdTight)) m_quality = 2;
     else if (mu.passed(reco::Muon::Selector::CutBasedIdLoose)) m_quality = 1;
     else m_quality = 0;
-
-    m_nhits = 0;
-    m_nbounds = 0;
 
     const reco::Track* muonTrack = 0;  
     if (mu.globalTrack().isNonnull()) muonTrack = mu.globalTrack().get();
     else if (mu.outerTrack().isNonnull()) muonTrack = mu.outerTrack().get();
     if (muonTrack) {
       reco::TransientTrack ttTrack = ttrackBuilder_->build(muonTrack);
-      
+      for (auto ch : GEMGeometry_->etaPartitions()) {
+	TrajectoryStateOnSurface tsos = propagator->propagate(ttTrack.outermostMeasurementState(),
+							      ch->surface());
+	if (!tsos.isValid()) continue;
+	GlobalPoint tsosGP = tsos.globalPosition();
+	const LocalPoint pos = ch->toLocal(tsosGP);
+	const LocalPoint pos2D(pos.x(), pos.y(), 0);
+	const BoundPlane& bps(ch->surface());
+
+	if (bps.bounds().inside(pos2D)) {
+	  m_nbounds++;
+	  
+	  auto gemid = ch->id();
+	  m_in_roll.push_back(gemid.roll());
+	  m_in_chamber.push_back(gemid.chamber());
+	  m_in_layer.push_back(gemid.layer());
+	  float in_x, in_y;
+	  in_x = tsosGP.x(); in_y = tsosGP.y();
+	  m_in_x.push_back(in_x); m_in_y.push_back(in_y);
+
+	  bool matchingGem = false;
+	  int nstrips = -1, firststrip = -1;
+	  float in_resx = -99, in_resy = -99, in_pullx = -99, in_pully = -99;
+	  float gemEta = +99.0, gemPhi = +99.0, in_gemx=-999, in_gemy=-999;
+	  auto recHitsRange = gemRecHits->get(gemid);
+	  auto gemRecHit = recHitsRange.first;
+	  for (auto hit = gemRecHit; hit != recHitsRange.second; ++hit) {
+	    auto gemGlob = ch->toGlobal(hit->localPosition());
+	    if (fabs(gemGlob.phi() - tsosGP.phi()) < gemPhi) {
+	      in_gemx = gemGlob.x();
+	      in_gemy = gemGlob.y();
+	      matchingGem = true;
+	      firststrip = hit->firstClusterStrip();
+	      nstrips = hit->clusterSize();
+
+	      LocalPoint && tsos_localpos = tsos.localPosition();
+	      LocalError && tsos_localerr = tsos.localError().positionError();
+	      LocalPoint && dethit_localpos = hit->localPosition();     
+	      LocalError && dethit_localerr = hit->localPositionError();
+	      in_resx = (dethit_localpos.x() - tsos_localpos.x());
+	      in_resy = (dethit_localpos.y() - tsos_localpos.y()); 
+	      in_pullx = (dethit_localpos.x() - tsos_localpos.x()) / 
+		std::sqrt(dethit_localerr.xx() + tsos_localerr.xx());
+	      in_pully = (dethit_localpos.y() - tsos_localpos.y()) / 
+		std::sqrt(dethit_localerr.yy() + tsos_localerr.yy());
+	    }
+	  }
+	  m_in_gemx.push_back(in_gemx); m_in_gemy.push_back(in_gemy);
+	  m_in_resx.push_back(in_resx); m_in_resy.push_back(in_resy);
+	  m_in_pullx.push_back(in_pullx); m_in_pully.push_back(in_pully);
+	  m_in_gemNStrips.push_back(nstrips); m_in_gemFirstStrip.push_back(firststrip);
+	  m_in_matchingGem.push_back(matchingGem);
+	}
+      }
     }
-  }
+
+    if (m_nhits > 0 or m_nbounds > 0) {
+      m_pt = mu.pt();
+      m_eta = mu.eta();
+      m_phi = mu.phi();
+      t_muon->Fill();
+    }
+  } // Muon Loop
 
   for (auto & gem : *gemRecHits) {
     ++b_nGEMHits;
@@ -218,8 +312,14 @@ AodSliceTestAnalysis::analyze(const edm::Event& iEvent, const edm::EventSetup& i
       b_strips.push_back(nstrip);
     }
 
+    auto roll = GEMGeometry_->etaPartition(detId);
+    auto globalPosition = roll->toGlobal(gem.localPosition());
+    b_x = globalPosition.x();
+    b_y = globalPosition.y();
+    b_z = globalPosition.z();
+    
     t_hit->Fill();
-  }
+  } // GEM Loop
 
   nMuonTotal += b_nMuons;
   nGEMTotal += b_nGEMHits;
